89812c47201156657de46c534087b22f444284bb
Vinegrad, Jake Rhys (Capgemini Flex) <jake.vinegrad@capgemini.com>
2020-05-19 09:44:26 +0100

    initial commit

---
+++ src/test/java/com/vinegrad/fruitapp/FruitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinegrad.fruitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
